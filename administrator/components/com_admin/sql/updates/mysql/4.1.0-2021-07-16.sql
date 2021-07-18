6f12aef16a2f0c115368c0c4f1f44e29aa412b1f
Jatin Salve <jasalve_b19@el.vjti.ac.in>
2021-07-18 21:42:34 +0530

    fixed SQL errors for com_guidedtours

--- administrator/components/com_admin/sql/updates/mysql/4.1.0-2021-07-16.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.1.0-2021-07-16.sql
@@ -49,5 +49,9 @@ CREATE TABLE IF NOT EXISTS `#__guidedtour_steps` (
   KEY `idx_tour` (`tour_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
-(0, 'com_guidedtours', 'component', 'com_guidedtours', '', 1, 1, 0, 1, '', '{}', '', 0, NULL, 0, 0);
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
+(0, 'com_guidedtours', 'component', 'com_guidedtours', '', 1, 1, 0, 0, 1, '', '{}', '', 0, NULL, 0, 0);
+
+
+
+
