2f3cff5dfe40d168333620481731a5fc0bf81e5d
Jatin Salve <jasalve_b19@el.vjti.ac.in>
2021-07-20 20:54:43 +0530

    removed outdated code from plugin

--- plugins/system/tour/tour.php
+++ plugins/system/tour/tour.php
@@ -1,27 +1,39 @@
 <?php
 
 /**
- * @package     Joomla.Plugin
- * @subpackage  System.Tourguide
+ * File Doc Comment_
+ * PHP version 5
  *
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @category  Component
+ * @package   Joomla.Administrator
+ * @author    Joomla! <admin@joomla.org>
+ * @copyright (C) 2013 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @link      admin@joomla.org
  */
+
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Uri\Uri;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
 
 /**
  * PlgSystemTour
  *
  * @since  __DEPLOY_VERSION__
  */
-class PlgSystemTour extends JPlugin
+
+class PlgSystemTour extends CMSPlugin implements SubscriberInterface
 {
+	/**
+	 * Load the language file on instantiation
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+
 	/**
 	 * Application object.
 	 *
@@ -29,13 +41,25 @@ class PlgSystemTour extends JPlugin
 	 * @since  __DEPLOY_VERSION__
 	 */
 	protected $app;
+	/**
+	 * Returns an array of events this subscriber will listen to.
+	 *
+	 * @return  array
+	 */
+	public static function getSubscribedEvents(): array
+	{
+		return [
+			'onBeforeRender' => 'onBeforeRender',
+		];
+	}
 
 	/**
-	 * Listener for the `onBeforeRender` event
-	 *
-	 * @return  void
-	 *
-	 * @since   __DEPLOY_VERSION__
+	 * Plugin method is the array value in the getSubscribedEvents method
+	 * The plugin then modifies the Event object (if it's not immutable)
+	 */
+
+	/**
+	 * @return void
 	 */
 	public function onBeforeRender()
 	{
@@ -46,7 +70,6 @@ class PlgSystemTour extends JPlugin
 			$toolbar = JToolbar::getInstance('toolbar');
 		}
 	}
-
 	/**
 	 * Listener for the `onBeforeCompileHead` event
 	 *
@@ -56,10 +79,14 @@ class PlgSystemTour extends JPlugin
 	 */
 	public function onBeforeCompileHead()
 	{
-		// Only going to run these in the backend for now
+		/**
+		 * @return void
+		 */
 		if ($this->app->isClient('administrator'))
 		{
-			// Todo
+			/**
+			 * TODO
+			 */
 		}
 	}
 }
