2466edf6da4e3318a17847b2c322b9b7c6873f2f
Jatin Salve <jasalve_b19@el.vjti.ac.in>
2021-07-30 12:06:27 +0530

    get access of 'data-id' by adding event listner

--- build/media_source/plg_system_tour/js/guide.js
+++ build/media_source/plg_system_tour/js/guide.js
@@ -3,46 +3,56 @@ Joomla = window.Joomla || {};
     document.addEventListener('DOMContentLoaded', function() {
         var mySteps = Joomla.getOptions('mySteps');
 
-        console.log(mySteps);
-        var tourDrop = document.getElementsByClassName('tourDrop');
+        // Accessing the 'data-id' 
+        // 1. for each tour element
+        // 2. get attribute id of each tour
+        // 3. assign the onClick event and do what you want with that attribute id
+        let btnGoods = document.querySelectorAll('.button-tour');
+        for (var i = 0; i < btnGoods.length; i++) {
+            btnGoods[i].addEventListener('click', function() {
+                var dataID = this.getAttribute('data-id');
+                console.log(dataID);
 
+                // Integrating ShepherdJS
+                const tour = new Shepherd.Tour({
+                    defaultStepOptions: {
+                        cancelIcon: {
+                            enabled: true
+                        },
+                        classes: 'class-1 class-2',
+                        scrollTo: { behavior: 'smooth', block: 'center' }
+                    }
+                });
 
-        // Integrating ShepherdJS
-        // const tour = new Shepherd.Tour({
-        //     defaultStepOptions: {
-        //         cancelIcon: {
-        //             enabled: true
-        //         },
-        //         classes: 'class-1 class-2',
-        //         scrollTo: { behavior: 'smooth', block: 'center' }
-        //     }
-        // });
+                tour.addStep({
+                    title: 'Creating a Shepherd Tour',
+                    text: `Creating a Shepherd tour is easy. too!\
+                    Just create a \`Tour\` instance, and add as many steps as you want.`,
+                    attachTo: {
+                        element: '.hero-example',
+                        on: 'bottom'
+                    },
+                    buttons: [{
+                            action() {
+                                return this.back();
+                            },
+                            classes: 'shepherd-button-secondary',
+                            text: 'Back'
+                        },
+                        {
+                            action() {
+                                return this.next();
+                            },
+                            text: 'Next'
+                        }
+                    ],
+                    id: 'creating'
+                });
+                document.getElementsByClassName(".button-tour").addEventListener('click', function() {
+                    tour.start();
+                });
 
-        // tour.addStep({
-        //     title: 'Creating a Shepherd Tour',
-        //     text: `Creating a Shepherd tour is easy. too!\
-        //            Just create a \`Tour\` instance, and add as many steps as you want.`,
-        //     attachTo: {
-        //         element: '.sidebar-item-title',
-        //         on: 'right'
-        //     },
-        //     buttons: [{
-        //             action() {
-        //                 return this.back();
-        //             },
-        //             classes: 'shepherd-button-secondary',
-        //             text: 'Back'
-        //         },
-        //         {
-        //             action() {
-        //                 return this.next();
-        //             },
-        //             text: 'Next'
-        //         }
-        //     ],
-        //     id: 'creating'
-        // });
-
-
+            });
+        }
     });
 }(Joomla, window));
