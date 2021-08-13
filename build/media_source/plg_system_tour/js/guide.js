76c494a2d4b4d09902f06c0860a5a6f981b38ff3
Jatin Salve <jasalve_b19@el.vjti.ac.in>
2021-08-13 19:28:21 +0530

    modified the base code of redirecting

--- build/media_source/plg_system_tour/js/guide.js
+++ build/media_source/plg_system_tour/js/guide.js
@@ -9,6 +9,7 @@ Joomla = window.Joomla || {};
             btnGoods[i].addEventListener('click', function() {
                 var dataID = this.getAttribute('data-id');
                 var mainID = obj.findIndex(x => x.id === dataID);
+                sessionStorage.setItem("tourid", mainID);
 
                 // Integrating ShepherdJS
                 const tour = new Shepherd.Tour({
@@ -46,16 +47,9 @@ Joomla = window.Joomla || {};
                     ],
                     id: 'creating'
                 });
-
-
-
                 // ----------------------------------------------------------------------------
                 var currentURL = window.location.href;
-                // var referrer = document.referrer;
-                // console.log(referrer);
-                var length = obj[mainID].steps.length
-                console.log(length);
-                // while () {
+
                 if (!sessionStorage.getItem("id")) {
                     for (index = 0; index < (obj[mainID].steps.length); index++) {
                         if (currentURL == obj[mainID].steps[index].url) {
@@ -88,45 +82,54 @@ Joomla = window.Joomla || {};
                         }
                     }
                     tour.start();
-
-                } else {
-                    var data = sessionStorage.getItem("id")
-                    var newIndex = obj[mainID].steps.findIndex(item => item.id == data);
-
-                    for (index = newIndex; index < ((obj[mainID].steps.length) - newIndex); newIndex++) {
-                        if (currentURL == obj[mainID].steps[index].url) {
-                            sessionStorage.setItem("id", obj[mainID].steps[index].id);
-
-                            tour.addStep({
-                                title: obj[mainID].steps[index].title,
-                                text: obj[mainID].steps[index].description,
-                                classes: 'intro-step shepherd-theme-arrows highlightClass',
-                                attachTo: {
-                                    element: obj[mainID].steps[index].target,
-                                    on: obj[mainID].steps[index].position
-                                },
-                                buttons: [{
-                                        action() {
-                                            return this.back();
-                                        },
-                                        classes: 'shepherd-button-secondary',
-                                        text: 'Back'
-                                    },
-                                    {
-                                        action() {
-                                            return this.next();
-                                        },
-                                        text: 'Next'
-                                    }
-                                ],
-                                id: obj[mainID].steps[index].id
-                            });
-                        }
-                    }
-                    tour.start();
                 }
-                // }
             });
+        }
+
+        //---------------------------------------------------------------------------------------------------
+        const tour = new Shepherd.Tour({
+            defaultStepOptions: {
+                cancelIcon: {
+                    enabled: true
+                },
+                classes: 'class-1 class-2 shepherd-theme-arrows',
+                scrollTo: { behavior: 'smooth', block: 'center' }
+            },
+        });
+        if (sessionStorage.getItem("tourid")) {
+            var newID = sessionStorage.getItem("id");
+            var newTourID = sessionStorage.getItem("tourid");
+
+            for (index = newID; index < (obj[newTourID].steps.length) - newTourID; index++) {
+
+
+                tour.addStep({
+                    title: obj[newTourID].steps[index].title,
+                    text: obj[newTourID].steps[index].description,
+                    classes: 'intro-step shepherd-theme-arrows highlightClass',
+                    attachTo: {
+                        element: obj[newTourID].steps[index].target,
+                        on: obj[newTourID].steps[index].position
+                    },
+                    buttons: [{
+                            action() {
+                                return this.back();
+                            },
+                            classes: 'shepherd-button-secondary',
+                            text: 'Back'
+                        },
+                        {
+                            action() {
+                                return this.next();
+                            },
+                            text: 'Next'
+                        }
+                    ],
+                    id: obj[newTourID].steps[index].id
+                });
+                tour.start();
+            }
+            sessionStorage.clear();
         }
     });
 }(Joomla, window));
