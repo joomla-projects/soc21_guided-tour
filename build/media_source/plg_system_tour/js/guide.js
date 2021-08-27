3e312ad0309f4fe2b6161256d45dce5c4f2e0d78
Jatin Salve <jasalve_b19@el.vjti.ac.in>
2021-08-27 19:52:28 +0530

    logic changes in tour plugin and added default field in plugin selector

--- build/media_source/plg_system_tour/js/guide.js
+++ build/media_source/plg_system_tour/js/guide.js
@@ -85,66 +85,89 @@ Joomla = window.Joomla || {};
         var currentURL = window.location.href;
         if (mainID) {
             for (index = 0; index < obj[mainID].steps.length; index++) {
-                if (currentURL == obj[mainID].steps[index].url) {
+                // if (currentURL == obj[mainID].steps[index].url) {
 
-                    var newInt = parseInt(objSteps[index].id);
-                    // console.log("Ananya");
-                    if (objSteps[index].multipage == '1') {
-                        sessionStorage.setItem('stepID', objSteps[index].multipage);
-                    }
-                    tour.addStep({
-                        title: obj[mainID].steps[index].title,
-                        text: obj[mainID].steps[index].description,
-                        classes: 'intro-step shepherd-theme-arrows',
-                        attachTo: {
-                            element: obj[mainID].steps[index].target,
-                            // showOn() {
-                            //     return document.querySelector(obj[mainID].steps[index].position) ? true : false;
-                            // }
-                            on: obj[mainID].steps[index].position,
-
-                        },
-                        buttons: [{
-                                action() {
-                                    return this.back();
-                                },
-                                classes: 'shepherd-button-secondary',
-                                text: 'Back'
-                            },
-                            {
-                                action() {
-                                    return this.next();
-                                },
-                                text: 'Next'
-                            }
-                        ],
-                        id: obj[mainID].steps[index].id,
-                        arrow: true,
-                        showOn: obj[mainID].steps[index].position,
-                        when: {
-                            show() {
-                                const currentStepElement = tour.currentStep.el;
-                                const header = currentStepElement.querySelector('.shepherd-header');
-                                const progress = document.createElement('span');
-                                progress.style['margin-right'] = '1px';
-                                progress.innerText = `${tour.steps.indexOf(tour.currentStep) + 1}/${tour.steps.length}`;
-                                header.insertBefore(progress, currentStepElement.querySelector('.shepherd-cancel-icon'));
-                            }
-                        },
-                        // beforeShowPromise: function() {
-                        //     return new Promise(function(resolve) {
-                        //         if (tour.steps.indexOf(obj[mainID].steps[index].title) == tour.steps.length - 1) {
-                        //             tour.trigger('complete');
-                        //             tour.done();
-                        //         } else {
-
-                        //             tour.show(index + 1);
-                        //         }
-                        //     });
-                        // },
-
-                    });
+                var newInt = parseInt(objSteps[index].id);
+                // console.log("Ananya");
+                if (objSteps[index].multipage == '1') {
+                    sessionStorage.setItem('stepID', objSteps[index].multipage);
                 }
+                tour.addStep({
+                    title: obj[mainID].steps[index].title,
+                    text: obj[mainID].steps[index].description,
+                    classes: 'intro-step shepherd-theme-arrows',
+                    attachTo: {
+                        element: obj[mainID].steps[index].target,
+
+                        on: obj[mainID].steps[index].position,
+
+
+                    },
+                    // beforeShowPromise: () => new Promise((resolve) => {
+
+                    //     let currentStep = '';
+                    //     setTimeout(() => {
+                    //         currentStep = tour.steps[index];
+                    //         if (currentStep.hasAttribute(obj[mainID].steps[index].position)) {
+                    //             tour.next();
+                    //         }
+                    //     }, 100);
+                    //     resolve();
+                    // }),
+                    getCurrentStepIndex: function() {
+                        return GuidedTourUtils.findWithAttr(Shepherd.activeTour.steps, "id", Shepherd.activeTour.getCurrentStep().id);
+                    },
+                    findWithAttr: function(array, attr, value) {
+                        for (var i = 0; i < array.length; i += 1) {
+                            if (array[i][attr] === value) {
+                                return i;
+                            }
+                        }
+                        return -1;
+                    },
+
+                    buttons: [{
+                            action() {
+                                return this.back();
+                            },
+                            classes: 'shepherd-button-secondary',
+                            text: 'Back'
+                        },
+                        {
+                            action() {
+                                return this.next();
+                            },
+                            text: 'Next'
+                        }
+                    ],
+                    id: obj[mainID].steps[index].id,
+                    arrow: true,
+                    showOn: obj[mainID].steps[index].position,
+                    when: {
+
+                        show() {
+                            const currentStepElement = tour.currentStep.el;
+                            const header = currentStepElement.querySelector('.shepherd-header');
+                            const progress = document.createElement('span');
+                            progress.style['margin-right'] = '1px';
+                            progress.innerText = `${tour.steps.indexOf(tour.currentStep) + 1}/${tour.steps.length}`;
+                            header.insertBefore(progress, currentStepElement.querySelector('.shepherd-cancel-icon'));
+                        }
+                    },
+                    // beforeShowPromise: function() {
+                    //     return new Promise(function(resolve) {
+                    //         if (tour.steps.indexOf(obj[mainID].steps[index].title) == tour.steps.length - 1) {
+                    //             tour.trigger('complete');
+                    //             tour.done();
+                    //         } else {
+
+                    //             tour.show(index + 1);
+                    //         }
+                    //     });
+                    // },
+
+                });
+                // }
 
             }
             // console.log(len);
@@ -161,9 +184,20 @@ Joomla = window.Joomla || {};
         //     return document.querySelector(obj[mainID].steps[index].position) ? true : false;
         // }
 
+        // if (mainID) {
+        //     for (index = 0; index < obj[mainID].steps.length; index++) {
+        //         tour.on('show', function() {
+        //             if (!myElement && previousStep.id === "the-previous-step") {
+        //                 tour.next();
+        //             } else if (!myElement && previousStep.id === "the-next-step") {
+        //                 tour.back();
+        //             }
+        //         });
+        //     }
+        // }
+        for (i = 0; i < obj[mainID].steps.length; i++) {
+
+        }
         tour.start();
-
-        console.log(currentURL)
-
     });
 }(Joomla, window));
